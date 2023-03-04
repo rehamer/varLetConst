c99bbdd639ee2f5af7d03d86a29c0b269df75b4c
rehamer <124471113+rehamer@users.noreply.github.com>
2023-03-04 16:29:38 -0500

    Add files via upload

---
+++ letAndConst.js
@@ -0,0 +1,23 @@
+//ES5 Global Constants
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+
+//ES2015 Global Constants
+/* Write an ES2015 Version */
+
+const PI = 3.14;
+let PI = 42;
+
+// Quiz
+// What is the difference between var and let?
+// var is a variable of windows while let is within a block
+
+// What is the difference between var and const?
+// var is a variable of windows while const is within a block
+
+// What is the difference between let and const?
+// let can be reassigned, while const is not
+
+// What is hoisting?
+// hoisting is related to var which gets initiated first before anything else in a program
